eef20ab2fe6d68a98db7dd3c7390287e051d6c03
shqie <visishqiponja@yahoo.com>
2015-07-13 00:06:19 -0400

    Create random account number method

--- data_gen.py
+++ data_gen.py
@@ -1,6 +1,16 @@
-def hello_world():
-    print("Hello World again")
+import random
+
     
+# Generate random account number    
+def random_account_number():
+     acct_num = random.randint(10**10, 10**11)
+     return acct_num
+
+ 
+ 
+
+for i in range (0, 10):
+    acct_num = random_account_number()   
+    print (acct_num)
     
-hello_world()
     
